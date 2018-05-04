c49718a07c631e16b59977c54e76d921c8f1880e
Housey <housebreaker1@gmail.com>
2018-05-04 19:37:56 +0300

    Create C# project and add packages

---
+++ MiniORM/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniORM
+{
+	using System.Data.SqlClient;
+
+	public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+	        var connectionString = "Server=.;Database=MiniORM;Integrated Security=True";
+        }
+    }
+}
